6820335c1635a0c057e7343423cfda983beb67fe
giuseppecavallo1989 <g.cavallo89@gmail.com>
2025-08-04 11:49:11 +0200

    init project and first commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+
+      //add comment
       theme: ThemeData(
         // This is the theme of your application.
         //
